0c5d4d3b60deeec8964a94f545b6f5e209002edc
Tunhoclaptrinh <146623045+Tunhoclaptrinh@users.noreply.github.com>
2024-03-28 23:10:04 +0700

    Add files via upload

---
+++ B23DCCC173_NguyenTienTuan_Script/DAY_01/main.js
@@ -0,0 +1,48 @@
+var day = 1;
+if (day == 2) {
+    alert("hôm nay là thứ "+ day);
+} else if (day == 3) {
+    alert("hôm nay là thứ "+ day);
+} else if (day == 4) {
+    alert("hôm nay là thứ "+ day);
+} else if (day == 5) {
+    alert("hôm nay là thứ "+ day);
+} else if (day == 6) {
+    alert("hôm nay là thứ "+ day);
+} else if (day == 7) {
+    alert("hôm nay là thứ "+ day);
+} else {
+    alert("hôm nay là chủ nhật");
+}
+
+switch(day){
+    case 2:{
+        alert("hôm nay là thứ 2");
+        break;
+    }
+    case 3:{
+        alert("hôm nay là thứ 3");
+        break;
+    }
+    case 4:{
+        alert("hôm nay là thứ 4");
+        break;
+    }
+    case 5:{
+        alert("hôm nay là thứ 5");
+        break;
+    }
+    case 6:{
+        alert("hôm nay là thứ 6");
+        break;
+    }
+    case 7:{
+        alert("hôm nay là thứ 7");
+        break;
+    }
+    default:{
+        set
+        alert("hôm nay là thứ chủ nhật");
+        break;
+    }
+}
---
+++ B23DCCC173_NguyenTienTuan_Script/DAY_01/switch_case.js
@@ -0,0 +1,28 @@
+var a = parseInt(prompt("Nhập số nguyên: "))
+
+switch (a) {
+    case 2 : 
+        document.write('Hôm nay là thứ hai');
+        break;
+    case 3 :
+        document.write('Hôm nay là thứ ba');
+        break;
+    case 4 :
+        document.write('Hôm nay là thứ tư');
+        break;
+    case 5 :
+        document.write('Hôm nay là thứ năm');
+        break;
+    case 6 :
+        document.write('Hôm nay là thứ sáu');
+        break;
+    case 7 :
+        document.write('Hôm nay là thứ bảy');
+        break;
+    case 8 :
+        document.write('Hôm nay là chủ nhật');
+        break;
+    default :
+        document.write('Hôm nay là thứ không biết');
+        break;
+}
